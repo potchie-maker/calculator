311338b945123c4395392caea9153b9d23c8b3c4
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-12-20 22:38:29 -0500

    Add event listeners

--- calculator.js
+++ calculator.js
@@ -16,7 +16,7 @@ function divide(a, b = 1) {
 
 let numA = 0;
 let numB = 0;
-let opp = '';
+let opp = "";
 
 function operate(a, op, b) {
   if (op === '+') return add(a, b);
@@ -24,3 +24,29 @@ function operate(a, op, b) {
   if (op === '*') return multiply(a, b);
   if (op === '/') return divide(a, b);
 }
+
+const screen = document.querySelector("#screen");
+
+const numPad = document.querySelectorAll(".num");
+numPad.forEach((num) => {
+  num.addEventListener("click", () => {
+    if (screen.textContent === "0") {
+      screen.textContent = num.textContent;
+    } else {
+      screen.textContent += num.textContent;
+    }
+    screen.scrollLeft = screen.scrollWidth;
+  });
+});
+
+const addBtn = document.querySelector("#add");
+addBtn.addEventListener("click", () => opp = "+");
+
+const subBtn = document.querySelector("#subtract");
+subBtn.addEventListener("click", () => opp = "-");
+
+const multiBtn = document.querySelector("#multiply");
+multiBtn.addEventListener("click", () => opp = "*");
+
+const divBtn = document.querySelector("#divide");
+divBtn.addEventListener("click", () => opp = "/");
