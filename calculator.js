98e5e9de839df26cc709afe1c126df89068e2d22
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-12-20 21:38:24 -0500

    Style basic calculator layout

--- calculator.js
+++ calculator.js
@@ -1,15 +1,26 @@
-function add(a, b) {
+function add(a, b = 0) {
   return a + b;
 }
 
-function subtract(a, b) {
+function subtract(a, b = 0) {
   return a - b;
 }
 
-function multiply(a, b) {
+function multiply(a, b = 1) {
   return a * b;
 }
 
-function divide(a, b) {
+function divide(a, b = 1) {
   return a / b;
+}
+
+let numA = 0;
+let numB = 0;
+let opp = '';
+
+function operate(a, op, b) {
+  if (op === '+') return add(a, b);
+  if (op === '-') return subtract(a, b);
+  if (op === '*') return multiply(a, b);
+  if (op === '/') return divide(a, b);
 }
